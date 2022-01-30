dd4add518800e13f45f420bc1b4bf24a5c9c1eee
Ksenia Typel <ksenia.typel@gmail.com>
2022-01-30 17:44:25 +0100

    zmienna kamien i papier

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let computerMove = `papier`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
